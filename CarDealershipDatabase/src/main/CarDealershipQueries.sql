ffa5c1e3a510b1e2a0a522bc66ad84ae38cf99b6
krosengr4 <rosenkev4@gmail.com>
2025-06-05 15:50:03 -0500

    Created queries to add dealerships and vehicles tables

--- CarDealershipDatabase/src/main/CarDealershipQueries.sql
+++ CarDealershipDatabase/src/main/CarDealershipQueries.sql
@@ -1,4 +1,25 @@
 DROP DATABASE car_dealership_db;
 CREATE DATABASE car_dealership_db;
+
 use car_dealership_db;
 
+CREATE table dealerships(
+	dealership_id int auto_increment,
+    name varchar(50),
+    address varchar(50),
+    phone varchar(12),
+    PRIMARY KEY (dealership_id)
+    );
+    
+CREATE table vehicles(
+	vehicle_id int,
+    vin int NOT NULL,
+    yearMade int,
+    make varchar(30),
+    model varchar(30),
+    color varchar(30),
+    vehicleType varchar(30),
+    odometer int,
+    price float,
+    PRIMARY KEY (vehicle_id)
+    );
